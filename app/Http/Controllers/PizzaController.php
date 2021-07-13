36b3a07063087b28df49c666a729d2430127fb24
Samandar <xkas2001@gmail.com>
2021-07-13 19:07:29 +0500

    Route in create blade php edit and some changes

--- app/Http/Controllers/PizzaController.php
+++ app/Http/Controllers/PizzaController.php
@@ -14,13 +14,18 @@ class PizzaController extends Controller
 //        $pizzas = Pizza::where('type', 'hawaiian')->get();
         $pizzas = Pizza::latest()->get();
 
-        return view('pizzas', [
+        return view('pizzas.index', [
             'pizzas' => $pizzas,
         ]);
     }
 
     public function show($id)
     {
-        return view('details', ['id' => $id]);
+        return view('pizzas.show', ['id' => $id]);
+    }
+
+    public function create()
+    {
+        return view('pizzas.create');
     }
 }
