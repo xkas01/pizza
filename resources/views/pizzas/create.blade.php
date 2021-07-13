fd2faacdc9948d8f8c516afd10d62152241b076f
Samandar <xkas2001@gmail.com>
2021-07-13 19:39:37 +0500

    Creating a web form and welcome blade in add link

--- resources/views/pizzas/create.blade.php
+++ resources/views/pizzas/create.blade.php
@@ -1,11 +1,26 @@
 @extends('layouts.layout')
 
 @section('content')
-    <div class="flex-center position-ref full-height">
-        <div class="content">
-            <div class="title m-b-md">
-                Add a new Pizza
-            </div>
-        </div>
+    <div class="wrapper create-pizza">
+        <h1>Create a New Pizza</h1>
+        <form action="/pizzas" method="post">
+            <label for="name">Your name:</label>
+            <input type="text" id="name" name="name">
+            <label for="type">Choose pizza type:</label>
+            <select name="type" id="type">
+                <option value="margarita">Margarita</option>
+                <option value="hawaiian">Hawaiian</option>
+                <option value="veg supreme">Veg Supreme</option>
+                <option value="volcano">Volcano</option>
+            </select>
+            <label for="base">Choose base type:</label>
+            <select name="base" id="base">
+                <option value="cheesy crust">Cheesy crust</option>
+                <option value="garlic crust">Garlic crust</option>
+                <option value="thin & crispy">Thin & crispy</option>
+                <option value="thick">Thick</option>
+            </select>
+            <input type="submit" value="Order Pizza">
+        </form>
     </div>
 @endsection
